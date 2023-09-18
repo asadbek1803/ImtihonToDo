2d4189af83a75b4de32e96c7e22801dc04a58a4b
asadbek <asadbekabdubannopov0408@gmail.com>
2023-09-18 16:20:34 +0500

    V.5.0: Created Todo model

--- home/models.py
+++ home/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Todo(models.Model):
+    status_s = (
+        ['bajarilmoqda', 'Bajarilmoqda'],
+        ['tugatildi', 'Tugatildi'],
+        ['to\'xtadi','To\'xtadi']
+    )
+    title = models.CharField(max_length=150)
+    time = models.DateTimeField()
+    description = models.TextField()
+    status = models.CharField(max_length=4, choices=status_s)
+
+
